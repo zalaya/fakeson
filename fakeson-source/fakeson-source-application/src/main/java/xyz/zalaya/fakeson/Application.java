cd1631e7a78d4fac9473f842322a1eddc717bc70
zalaya <enol.corzal@gmail.com>
2024-06-17 23:13:48 +0200

    feat(structure): add the structure for the project, including the spring boot module and a docker module

---
+++ fakeson-source/fakeson-source-application/src/main/java/xyz/zalaya/fakeson/Application.java
@@ -0,0 +1,13 @@
+package xyz.zalaya.fakeson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
