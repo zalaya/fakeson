7509353c6c19569206f57ddf288599fa3a936fc2
zalaya <enol.corzal@gmail.com>
2024-06-23 22:21:31 +0200

    feat(structure): add the database module for the products api

--- fakeson-scripts/src/main/resources/databases/products/tables.sql
+++
@@ -1,8 +0,0 @@
-USE fakeson_source_products;
-
-CREATE TABLE IF NOT EXISTS products (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    price DECIMAL(10, 2)
-);
