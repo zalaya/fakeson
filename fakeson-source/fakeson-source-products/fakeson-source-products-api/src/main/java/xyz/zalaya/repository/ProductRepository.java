95aada9e63d19a6e807495aec79f3320d0e87cae
zalaya <enol.corzal@gmail.com>
2024-06-27 01:00:06 +0200

    feat(structure): change the monolithic architecture for a hexagonal one with 3 layers

--- fakeson-source/fakeson-source-products/fakeson-source-products-api/src/main/java/xyz/zalaya/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package xyz.zalaya.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.zalaya.model.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
-}
